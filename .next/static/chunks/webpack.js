4eb7be4dbe394a24ae10fd97777152f2f2a0bc14
jamesmoore24 <jame.moore24@gmail.com>
2025-03-25 22:32:44 -0400

    layout changed for icons

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "c585625acb31b5a3"; }
+/******/ 		__webpack_require__.h = function() { return "95fc204fa58e0e46"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,25 +1,22 @@
 import "./globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import Icon from "@/public/icon.png";
+import Icon16 from "@/public/icon16.png";
+import Icon32 from "@/public/icon32.png";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "BallKnower",
   description: "An open-source AI agent for sports analysis",
-  openGraph: {
-    title: "BallKnower - Sports Betting",
-    description: "An open-source AI agent for sports analysis",
-    url: "https://ballknower.bet",
-    type: "website",
-    images: [
-      {
-        url: "https://ballknower.bet/images/logo.png",
-        width: 1200,
-        height: 630,
-        alt: "BallKnower Logo",
-      },
+  icons: {
+    icon: [
+      { url: Icon.src },
+      { url: Icon16.src, sizes: "16x16", type: "image/png" },
+      { url: Icon32.src, sizes: "32x32", type: "image/png" },
     ],
+    apple: [{ url: "/apple-touch-icon.png" }],
   },
 };
 
