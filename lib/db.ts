206a661be1beddd043fdcf72ffaf8c15bcc1ea44
jamesmoore24 <jame.moore24@gmail.com>
2025-03-25 23:43:48 -0400

    logging change

--- lib/db.ts
+++ lib/db.ts
@@ -31,7 +31,6 @@ export function getDatabase() {
 
 export function getAllEntries() {
   const db = getDatabase();
-  console.log("db", db);
   try {
     console.log("SELECTING ALL ENTRIES");
     return db.prepare("SELECT * FROM bets ORDER BY created_at DESC").all();
