50ef00ae626e7824b192b0146a3a9fffc7e29ebf
jamesmoore24 <jame.moore24@gmail.com>
2025-03-25 22:55:22 -0400

    .next folder ignore and sharp added to dependencies

--- .next/server/webpack-runtime.js
+++ .next/server/webpack-runtime.js
@@ -125,7 +125,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	(() => {
-/******/ 		__webpack_require__.h = () => ("7b4758bb6d928f2b")
+/******/ 		__webpack_require__.h = () => ("f631a7c8852723aa")
 /******/ 	})();
 /******/ 	
 /******/ 	/* webpack/runtime/hasOwnProperty shorthand */
